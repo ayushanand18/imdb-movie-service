0d7d0b54bf12d4e0494256008b88b70730cd99cd
Ayush Anand <36472216+ayushanand18@users.noreply.github.com>
2024-04-15 07:58:01 +0000

    fix: should fix errors?

--- next-frontend/app/Body.tsx
+++ next-frontend/app/Body.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import Link from "next/link";
 
@@ -19,51 +19,27 @@ const Body: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center h-screen body">
-      <h1 className="text-5xl font-bold mb-8 mt-40 text-white">
-        Welcome to our movie database
+      <h1 className="text-[5em] leading-[1.2em] shadow-xl backdrop-blur p-4 font-extrabold my-12 mt-40 text-white">
+        Welcome To Our <br />
+        Movie Database
       </h1>
-      <div className="flex items-center">
-        <div className="relative">
-          <input
-            type="text"
-            placeholder="Search"
-            className="border-2 border-gray-300 rounded-full py-2 px-4 focus:outline-none focus:border-gray-500"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <span className="absolute right-3 top-1/2 transform -translate-y-1/2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 15l4.5 4.5M7 13l-4.5 4.5M17 7l4.5-4.5M7 7l-4.5-4.5"
-              />
-            </svg>
-          </span>
-        </div>
-        <button
-          onClick={handleSearch}
-          className="ml-4 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full"
-        >
-          Get Movies
-        </button>
-      </div>
-
-      {/* Display movies */}
-      <div className="mt-8">
-        <h2 className="text-2xl font-bold mb-4">Search Results:</h2>
-        <ul>
-          {movies.map((movie) => (
-            <li key={movie.id}>{movie.title}</li>
-          ))}
-        </ul>
+      <div className="flex items-center text-xl my-4">
+        <Link href="/search">
+          <button
+            onClick={handleSearch}
+            className="ml-4 bg-red-500 hover:bg-red-600 text-white font-bold p-4 px-12 rounded-full"
+          >
+            Get Movies
+          </button>
+        </Link>
+        <Link href="/charts">
+          <button
+            onClick={handleSearch}
+            className="ml-4 bg-slate-500 hover:bg-slate-600 text-white font-bold p-4 px-12 rounded-full"
+          >
+            View Charts
+          </button>
+        </Link>
       </div>
     </div>
   );
